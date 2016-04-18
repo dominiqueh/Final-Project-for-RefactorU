4f8581bb9b085b1a2546d65af877164481d749d8
DominiqueH <Student@Mac-mini-3.local>
2016-04-18 11:10:06 -0600

    adding to base setup for server.js

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Final-Project-for-RefactorU
 
-Technologies Used: 
+Technologies Used:
 Built a REST API for handling CRUD for victim entries.
+
+Later Implementations:
+Facial Recognition
+GULP
--- server.js
+++ server.js
@@ -3,12 +3,33 @@
 //==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==
 // Require needed packages
 // Define our app using express while requiring and calling express
-var app         = require('express')(),
-    bodyParser  = require('body-parser'),
-    logger      = require('morgan'),
-    cors        = require('cors')
+var express     = require('express'),
+var app         = express(),
+var bodyParser  = require('body-parser'),
+var logger      = require('morgan'),
+var cors        = require('cors')
 
+//for every request coming in apply the logger
+app.use(logger('dev'))
 // Configure app to use bodyParser()
 // This will retrieve data from a POST HTTP request
-app.use(bodyParser.urlEncoded({ extended : true })) // stringifies query data
+app.use(bodyParser.urlencoded({ extended : true })) // stringifies query data
 app.use(bodyParser.json()) // parses data to json
+app.use(cors()) // cors errors
+
+var port = process.env.PORT || 3000 // set the PORT
+//==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==
+// ROUTES FOR API
+//==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==
+var router = express.Router()
+
+router.get('/', function(req,res) {
+  res.json({ message: "It's ALIVE!!!! API is up!"})
+})
+
+app.use('/api', router)
+//==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==
+// STARTING THE SERVER
+//==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==
+app.listen(port)
+console.log('Magic is happening on port ' + port)
