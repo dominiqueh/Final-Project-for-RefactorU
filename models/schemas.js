2d7c06700e5039a677b8bfd2ce0dc3b7f696dadf
dominiqueh <dominiquehorner@gmail.com>
2016-04-20 17:52:51 -0600

    set up both schemas for schemas.js

--- models/schemas.js
+++ models/schemas.js
@@ -1,21 +1,21 @@
-//===+==+=+=+==+===*===+==+=+=+==+===*===+==+=+=+==+====
+//===+==+=+=+==+===*===+==+=+=+==+===*===+==+=+=+==+====//
 // BASE SETUP FOR SCHEMAS
-//===+==+=+=+==+===*===+==+=+=+==+===*===+==+=+=+==+====
+//===+==+=+=+==+===*===+==+=+=+==+===*===+==+=+=+==+====//
 
 var mongoose      = require('mongoose'),
-    bcrypt      = require('bcrypt'),
+    bcrypt        = require('bcryptjs'),
     Schema        = mongoose.Schema,
 
-//===+==+=+=+==+===*===+==+=+=+==+===*===+==+=+=+==+====
+//===+==+=+=+==+===*===+==+=+=+==+===*===+==+=+=+==+====//
 // USER SCHEMA
-//===+==+=+=+==+===*===+==+=+=+==+===*===+==+=+=+==+====
+//===+==+=+=+==+===*===+==+=+=+==+===*===+==+=+=+==+====//
+
     userSchema  = new Schema({
       // don't add an id since mongo automatically adds one
-      name      : String,
-      email     : String,
-      password  : String,
-      admin     : Boolean,
-    })
+      email     : {type: String, required: true},
+      password  : {type: String, required: true},
+      admin     : Boolean
+    }),
 
     userSchema.pre('save', function(next){
       var user = this // defining the scope to be the user using this method
@@ -26,15 +26,30 @@ var mongoose      = require('mongoose'),
 
     userSchema.methods.checkPassword = function(pw){
       var user = this
-      bcrypt.compareSync(pw, user.password)
+      return bcrypt.compareSync(pw, user.password) // ensures user enters the correct password
     }
 
 
-//===+==+=+=+==+===*===+==+=+=+==+===*===+==+=+=+==+====
+//===+==+=+=+==+===*===+==+=+=+==+===*===+==+=+=+==+====//
 // ENTRY SCHEMA
-//===+==+=+=+==+===*===+==+=+=+==+===*===+==+=+=+==+====
+//===+==+=+=+==+===*===+==+=+=+==+===*===+==+=+=+==+====//
 
+  entrySchema = new Schema ({
+    name        : String,
+    gender      : String,
+    age         : Number,
+    photo       : String,
+    dateListed  : Date,
+    centerListed: String,
+    lastSeen    : String,
+    report      : String,
+    sandw       : String
+  })
+
+//===+==+=+=+==+===*===+==+=+=+==+===*===+==+=+=+==+====//
+// CONNECT ALL SCHEMAS
+//===+==+=+=+==+===*===+==+=+=+==+===*===+==+=+=+==+====//
 module.exports = {
-      User = mongoose.model('User', userSchema),
-        // add entry model
+      User  = mongoose.model('User', userSchema),
+      Entry = mongoose.model('Entry', entrySchema)
 }
