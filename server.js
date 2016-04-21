c5a4cab89849eb54b941606768e4cc412fb2646f
dominiqueh <dominiquehorner@gmail.com>
2016-04-20 19:31:33 -0600

    added onto server.js, nodemon throws up err

--- server.js
+++ server.js
@@ -8,45 +8,18 @@ var express     = require('express'),
     logger      = require('morgan'),
     cors        = require('cors'),
     mongoose    = require('mongoose'),
-    // bcrypt      = require('bcrypt')
+    routes      = require('./api_routes'),
     port        = process.env.PORT || 8080,
-    Person      = require('./models/missingPerson.js').Person
+    databaseURL = "mongodb://localhost:27017/oneBoard"
 
 //==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==
 // CONNECT TO DATABASE
 //==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==
+
 mongoose.connect( 'mongodb://admin:munster47@ds057244.mlab.com:57244/oneboard', function(err){
   if(err) console.log(err)
   console.log('connected to OneBoard MONGODS')
 })
-
-//==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==
-// APPLY MIDDLEWARE
-//==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==
-app.use(logger('dev')) // applies log to each request coming in
-app.use(bodyP.urlencoded({ extended : true })) // stringifies query data
-app.use(bodyP.json()) // parses data to json
-app.use(cors()) // cors errors
-
-//==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==
-// ROUTES FOR API
-//==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==
-var router = express.Router()
-
-//middleware for all requests
-router.use(function(req,res, next){
-  console.log('the use router is working')
-  next(); // keeps moving on to the next route
-})
-
-//test route that everything is working
-router.get('/', function(req,res) {
-  res.json({ message: "It's ALIVE!!!! API is up!"})
-})
-
-router.route('')
-
-app.use('/api/v1/', router)
 
 //==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==+=+=+==
 // STARTING THE SERVER
